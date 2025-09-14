63f74b9e79abc21510b88dc64ad967dc663b3922
Dadzial <166065633+Dadzial@users.noreply.github.com>
2025-09-14 17:21:52 +0200

    Update Downloader.py

--- Downloader.py
+++ Downloader.py
@@ -1,33 +1,15 @@
-<<<<<<< HEAD
-from PySide6 import QtCore
-import yt_dlp
-import os
-
-class Downloader(QtCore.QObject):
-    finished = QtCore.Signal(str)
-=======
 from PySide6.QtWidgets import QFileDialog
 from PySide6.QtCore import QObject, Signal, Slot
 import yt_dlp
 
 class Downloader(QObject):
     finished = Signal(str)
->>>>>>> 8080e82 (end of project)
 
     def __init__(self, parent=None):
         super().__init__(parent)
 
-<<<<<<< HEAD
-    @QtCore.Slot(str)
-    def download(self, url: str):
-        ydl_opts = {
-            "outtmpl": "/home/damiand/Pobrane/%(title)s.%(ext)s",
-            "format": "best",
-        }
-
-=======
-    @Slot(str,str,str)
-    def download(self, url: str, format:str ,path :str):
+    @Slot(str, str, str)
+    def download(self, url: str, format: str, path: str):
 
         format_map = {
             "mp4": "bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4",
@@ -38,29 +20,22 @@ class Downloader(QObject):
             "outtmpl": f"{path}/%(title)s.%(ext)s",
             "format": format_map.get(format, "best")
         }
+
         if format == "mp3":
             ydl_opts["postprocessors"] = [{
                 "key": "FFmpegExtractAudio",
                 "preferredcodec": "mp3",
                 "preferredquality": "192"
-        }]
-<<<<<<< HEAD
->>>>>>> 8080e82 (end of project)
-=======
+            }]
 
->>>>>>> 46b44e9 (end of project)
         try:
             with yt_dlp.YoutubeDL(ydl_opts) as ydl:
                 ydl.download([url])
             self.finished.emit("Download complete!")
         except Exception as e:
             self.finished.emit(f"Error: {e}")
-<<<<<<< HEAD
-=======
+
     @Slot(result=str)
     def choose_folder(self):
         folder = QFileDialog.getExistingDirectory(None, "Select folder")
         return folder if folder else ""
-
-
->>>>>>> 8080e82 (end of project)
